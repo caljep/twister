ada84ff00879569aad0fcbc425156a122c29a06b
Calvin Jepson <calvin@particle.io>
2018-11-09 14:22:04 -0800

    Adding interrupt timers

--- src/twister.cpp
+++ src/twister.cpp
@@ -7,6 +7,7 @@
  * Dce:
  */
 int toggleDebug(String command);
+void setInterruptTime();
 void setup();
 void loop();
 #line 7 "/Users/calparticle/Desktop/repos/twister/src/twister.ino"
@@ -18,6 +19,11 @@ int newReading;
 int size = sizeof(readings)/sizeof(readings[0]);
 int accum;
 
+time_t initialTime;
+time_t initialMillis;
+time_t lastTime = 0;
+volatile time_t interruptTimeRelative = 0;
+
 Servo myServo;
 
 int toggleDebug(String command) {
@@ -26,12 +32,22 @@ int toggleDebug(String command) {
   return ret;
 }
 
+void setInterruptTime() {
+  interruptTimeRelative = millis() - initialMillis;
+}
+
 // setup() runs once, when the device is first turned on.
 void setup() {
   // Put initialization like pinMode and begin functions here.
   debug = false;
   myServo.attach(servoPin);
   Particle.function("toggleDebug",toggleDebug);
+  
+  Serial.printlnf("Boot-up time: %s", Time.timeStr().c_str());
+  initialTime = Time.now(); 
+  initialMillis = millis();
+
+  attachInterrupt(D2, setInterruptTime, CHANGE);
 }
 
 // loop() runs over and over again, as quickly as it can execute.
@@ -41,15 +57,22 @@ void loop() {
   counter++;
 
   // if (debug) {
-  // Serial.printf("size: %d, counter: %d\n",size, counter);
-  // Serial.printf("nr: %d\n",newReading);
-  Serial.printf("%d, %d, %d, %d\n", readings[0], readings[1], readings[2], readings[3]);
+  //   Serial.printf("size: %d, counter: %d\n",size, counter);
+  //   Serial.printf("nr: %d\n",newReading);
+  //   Serial.printf("%d, %d, %d, %d\n", readings[0], readings[1], readings[2], readings[3]);
+  //   Serial.printf("avg: %i\n", accum/size);
   // }
+  
   accum = 0;
   for (int i=0; i<size; i++) {
     accum += readings[i];
   }
-  Serial.printf("avg: %i\n", accum/size);
+  //Serial.printlnf("Current time)
+  if (lastTime != interruptTimeRelative) {
+    Serial.printf("Latest Interrupt: %s\n", Time.timeStr(interruptTimeRelative/1000+initialTime).c_str());
+    lastTime = interruptTimeRelative;
+  }
+  // Serial.printf("current time: %s\n",Time.timeStr(initialTime + interruptTimeRelative/1000));
   myServo.write(accum/size);
   delay(30);
 }
--- src/twister.ino
+++ src/twister.ino
@@ -12,6 +12,11 @@ int newReading;
 int size = sizeof(readings)/sizeof(readings[0]);
 int accum;
 
+time_t initialTime;
+time_t initialMillis;
+time_t lastTime = 0;
+volatile time_t interruptTimeRelative = 0;
+
 Servo myServo;
 
 int toggleDebug(String command) {
@@ -20,31 +25,41 @@ int toggleDebug(String command) {
   return ret;
 }
 
+void setInterruptTime() {
+  interruptTimeRelative = millis() - initialMillis;
+}
+
 // setup() runs once, when the device is first turned on.
 void setup() {
-  // Put initialization like pinMode and begin functions here.
   debug = false;
   myServo.attach(servoPin);
   Particle.function("toggleDebug",toggleDebug);
+  
+  Serial.printlnf("Boot-up time: %s", Time.timeStr().c_str());
+  initialTime = Time.now(); 
+  initialMillis = millis();
+
+  attachInterrupt(D2, setInterruptTime, CHANGE);
 }
 
-// loop() runs over and over again, as quickly as it can execute.
 void loop() {
   newReading = map(analogRead(potPin),0,4095,0,180);
+
+  //To smooth out the noise in the analog reading, I'm taking an avg
   readings[counter % size] = newReading;
   counter++;
-
-  // if (debug) {
-  // Serial.printf("size: %d, counter: %d\n",size, counter);
-  // Serial.printf("nr: %d\n",newReading);
-  // Serial.printf("%d, %d, %d, %d\n", readings[0], readings[1], readings[2], readings[3]);
-  // }
-  
   accum = 0;
   for (int i=0; i<size; i++) {
     accum += readings[i];
   }
-  Serial.printf("avg: %i\n", accum/size);
+  //Write the avg value of the Pot. to the servo
   myServo.write(accum/size);
+
+  //Reading the interrupt time on the D2 pin and printing it to serial. 
+  if (lastTime != interruptTimeRelative) {
+    Serial.printf("Latest Interrupt: %s\n", Time.timeStr(interruptTimeRelative/1000+initialTime).c_str());
+    lastTime = interruptTimeRelative;
+  }
+
   delay(30);
 }
